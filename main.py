00bcb7ae25c3012bdb1f2e426865ada07e37a2fd
AbdullahNaseer123 <abdullahallies154@gmail.com>
2024-01-14 21:27:32 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,99 @@
+# num1 = int(input("number 1: "))
+# num2 = int(input("number 2: "))
+# calc = input("operator: ")
+#
+# if calc == "+":
+#     print (num1 + num2)
+# elif calc == "-":
+#     print (num1 - num2)
+# elif calc == "*":
+#     print (num1 * num2)
+# elif calc == "/":
+#     print (num1 / num2)
+# else:
+#     print ("wrong operators")
+#
+# calc1 = input("operator: ")
+#
+# if calc1 == "+":
+#     print (num1 + num2)
+# elif calc1 == "-":
+#     print (num1 - num2)
+# elif calc1 == "*":
+#     print (num1 * num2)
+# elif calc1 == "/":
+#     print (num1 / num2)
+# else:
+#     print ("wrong operators")
+#
+# calc2 = input("operator: ")
+#
+# if calc2 == "+":
+#     print (num1 + num2)
+# elif calc2 == "-":
+#     print (num1 - num2)
+# elif calc2 == "*":
+#     print (num1 * num2)
+# elif calc2 == "/":
+#     print (num1 / num2)
+# else:
+#     print ("wrong operators")
+
+# x = input("enter a word: ")
+#
+# if x == x[::-1]:
+#     print ("this word is pallendrom")
+# else:
+#     print ("this word is not a pallendrom")
+
+# x = int(input("enter a num: "))
+# y = 7
+# print (x == y)
+# x = int(input("enter a num: "))
+# y = % 2
+# print (y)
+
+# x = input('enter you name: ')
+# y = input("experience in programming: ")
+# z = int(input("age: "))
+#
+# print (z >= 18)
+# age = 60 - z
+# print ("Years till Retirement: ", age, "years")
+# print (f" Hello {x} Welcome to our programming class you have a { z >= 18 } age to join our class, you have got {y} years of programming experience and still you have {age} years before your retirement.")
+
+# x = int(input("enter principle amount: "))
+# y = float(input("enter interest rate (in percentage): "))
+# z = int(input("enter time period (in years): "))
+#
+# print ('simple_interest = ', x * y * z)
+#
+# print ("compound interest = ", (x * (1 + y) ** z) - x)
+
+# x = int(input("enter a num: "))
+# a = (x % 2)
+# print ( a == 0 )
+
+# x = int(input("enter a num: "))
+#
+# if x % 2 == 1:
+#     print ("The Number is odd")
+# else:
+#     print ("The Number is even")
+#
+# x2 = int(input("enter a num: "))
+#
+# if x2 % 2 == 1:
+#     print ("The Number is odd")
+# else:
+#     print ("The Number is even")
+
+x = int(input("enter how many times to write it:"))
+y = input("enter the text you want to repeat: ")
+for x in range (x):
+    print (y)
+
+
+
+
+
